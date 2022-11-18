2c98d36e8096f36d7f53def2b60cc4bf64a95b4c
RicarTang <357965604@qq.com>
2022-11-18 14:08:02 +0800

    wallet manage

--- cypress/e2e/3.manage_wallet.cy.js
+++ cypress/e2e/3.manage_wallet.cy.js
@@ -1,8 +1,23 @@
-describe.skip('Manage wallet',()=>{
-    before(()=>{
-        cy.restoreWallet()  // 前置，恢复钱包
+import walletManageEle from '../fixtures/ele/walletManageEle.json'
+import restoreWalletJson from '../fixtures/data/restore_wallet.json'
+
+describe('Manage wallet', () => {
+    before(() => {
+        // cy.restoreWallet()  // 前置，恢复钱包
     })
-    context('test',()=>{
-       
+    context('identityWallet Manage', () => {
+        // 导出身份钱包助记词
+        it.skip('identityWalletReportMnemonic', () => {
+            cy.get(walletManageEle.walletList).click()
+            cy.get(walletManageEle.walletManage).click()
+            cy.get(walletManageEle.identityManage).click()
+            cy.get(walletManageEle.identityWalletReportMnemonic).click()
+            cy.get(walletManageEle.verifyPassword).type(restoreWalletJson.restoreSuccess.walletPassword)
+            cy.get(walletManageEle.verifyPasswordConfirm).click()
+            cy.url().should('contains','mnemonic-backup-tips?export=true')
+        })
+        it('test',()=>{
+            cy.visit('/mnemonic/manage-identity-wallet')
+        })
     })
 })
